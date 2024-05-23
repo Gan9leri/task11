bd7506eda509aa5e41181f1f91fd413e9390fecc
AOvsyannikov <aovsyannikov@it-one.ru>
2024-05-24 00:38:40 +0300

    updated RegistrationPage.java

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,10 +1,8 @@
 package pages;
-
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.StateAndCityComponent;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
