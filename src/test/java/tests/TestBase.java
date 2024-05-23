5b89da9a2af731ddd5deff1561d2cd8bf47e6dab
AOvsyannikov <aovsyannikov@it-one.ru>
2024-05-24 00:14:53 +0300

    updated TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,14 +1,19 @@
 package tests;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         //Configuration.holdBrowserOpen = true;
     }
+
+
 }
