a9b85d65690deee06ebac7d8e08f229b7b146cdd
AOvsyannikov <aovsyannikov@it-one.ru>
2024-05-24 00:31:10 +0300

    updated TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,9 @@
 package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -15,5 +17,12 @@ public class TestBase {
         //Configuration.holdBrowserOpen = true;
     }
 
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
 
+    }
 }
